7a7cfa4abb28a3dd0d07719fa15db88dee52e473
Adrian Sadłocha <adrian@sadlocha.eu>
2021-04-16 03:20:07 +0200

    Print "Hello world!" as a welcome message
    `0x7C00` is a common bootloader location. We specify this so an
    assembler can calculate proper addreses (in this case, the address of
    `hello`).

--- boot.asm
+++ boot.asm
@@ -1,4 +1,22 @@
+org 0x7C00
+
+xor ax, ax
+mov ds, ax
+
+mov ah, 0x13
+mov al, 0x01
+xor bh, bh
+mov bl, 0x07
+mov cx, [helloLen]
+mov dh, 0
+mov dl, 0
+mov bp, hello
+int 0x10
+
 hlt
+
+hello db "Hello world!", 0x0D, 0x0A
+helloLen db $ - hello
 times 510-($-$$) db 0
 db 0x55
 db 0xAA
