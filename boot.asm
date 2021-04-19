4e44273b44c6aab8cc6434b1a283ebe92749bb38
Adrian Sadłocha <adrian@sadlocha.eu>
2021-04-19 03:20:53 +0200

    Optimize code size by using immediate value

--- boot.asm
+++ boot.asm
@@ -7,7 +7,7 @@ mov ah, 0x13
 mov al, 0x01
 xor bh, bh
 mov bl, 0x07
-mov cx, [helloLen]
+mov cx, helloLen
 mov dh, 0
 mov dl, 0
 mov bp, hello
@@ -16,7 +16,7 @@ int 0x10
 hlt
 
 hello db "Hello world!", 0x0D, 0x0A
-helloLen db $ - hello
+helloLen equ $ - hello
 times 510-($-$$) db 0
 db 0x55
 db 0xAA
