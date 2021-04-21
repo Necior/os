e4e1a31cd2ceea88b77fa6b1748953a05dbb6e17
Adrian Sadłocha <adrian@sadlocha.eu>
2021-04-22 00:54:37 +0200

    Print "Hello world!" three times

--- boot.asm
+++ boot.asm
@@ -3,15 +3,21 @@ org 0x7C00
 xor ax, ax
 mov ds, ax
 
+mov dh, 0
+
+loop:
 mov ah, 0x13
 mov al, 0x01
 xor bh, bh
 mov bl, 0x07
 mov cx, helloLen
-mov dh, 0
 mov dl, 0
 mov bp, hello
 int 0x10
+
+add dh, 0x01
+cmp dh, 0x03
+jne loop
 
 hlt
 
